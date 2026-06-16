13074a4865e27bfb0d719f3127f8a52b9a45cdfa
Fkernel653 <unkown11250@proton.me>
2026-06-16 11:51:43 +0500

    Update cli.py

--- arg_kiss/cli.py
+++ arg_kiss/cli.py
@@ -201,7 +201,7 @@ class CLI:
         )
 
         if result is not None:
-            sys.stdout.write(result + "\n")
+            sys.stdout.write(str(result) + "\n")
 
     def __call__(self, args: List[str] | None = None) -> None:
         return self.run(args)
